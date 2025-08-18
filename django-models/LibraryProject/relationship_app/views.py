1d5b653c7da8377bb0fd2bdf79baef9550d0033f
Chikizy06 <Chijinduonyemelukwe001@gmail.com>
2025-08-18 23:48:33 +0100

    1c4

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,6 +12,7 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
